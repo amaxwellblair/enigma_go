2cfe7b7446cffdf24bb5652e0e03f3d257fe3017
Allan <amaxwellblair@gmail.com>
2016-01-23 12:18:17 -0700

    initial commit created encrypt functions rotate and offset

---
+++ encrypt/encrypt.go
@@ -0,0 +1,54 @@
+package encrypt
+
+import (
+  "strconv"
+)
+
+type Encrypt struct {
+    Key string
+    Date string
+}
+
+func New() (*Encrypt) {
+    return new(Encrypt)
+}
+
+func pow(base int, power int) (value int) {
+    for i := 0; i < power; i++ {
+        if i == 0 {
+            value = base
+        } else {
+            value *= base
+        }
+    }
+    return
+}
+
+func (e *Encrypt) Rotate(number int) (tumbler int) {
+    chars := []rune(e.Key)
+    if (number > (len(chars)-2)) {
+        return
+    }
+    tens, _ := strconv.Atoi(string(chars[number]))
+    ones, _ := strconv.Atoi(string(chars[number+1]))
+    tumbler = tens*10 + ones
+    return
+}
+
+func (e *Encrypt) dateConv()(dateNums string) {
+    dateInt, _ := strconv.Atoi(e.Date)
+    dateRune := []rune(strconv.Itoa(pow(dateInt,2)))
+    runeLength := len(dateRune)
+    dateNums = string(dateRune[(runeLength-4):])
+    return
+}
+
+func (e *Encrypt) Offset(index int) (tumbler int) {
+    encodedDate := e.dateConv()
+    dateRune := []rune(encodedDate)
+    if (len(dateRune)-1) < index {
+        return
+    }
+    tumbler, _ = strconv.Atoi(string(dateRune[index]))
+    return
+}
---
+++ encrypt/encrypt_test.go
@@ -0,0 +1,85 @@
+package encrypt
+
+import (
+  "testing"
+)
+
+func TestCreateEncryptStruct(t *testing.T)  {
+    var e interface{}
+    e = New()
+    switch e.(type) {
+      case *Encrypt:
+
+      default:
+        t.Log("The encrypt struct isn't being created correctly")
+        t.Fail()
+    }
+}
+
+func TestZeroRotatingOnAGivenIndex(t *testing.T)  {
+    e := New()
+    e.Key = "12345"
+    rotation := e.Rotate(0)
+    if rotation != 12 {
+      t.Log("The 0 rotation didn't yield the proper number")
+      t.Fail()
+    }
+}
+
+func TestOneRotatingOnAGivenIndex(t *testing.T)  {
+    e := New()
+    e.Key = "12345"
+    rotation := e.Rotate(1)
+    if rotation != 23 {
+      t.Log("The 1 rotation didn't yield the proper number")
+      t.Fail()
+    }
+}
+
+func TestArotationIsOutOfRange(t *testing.T) {
+    e := New()
+    e.Key = "12345"
+    rotation := e.Rotate(4)
+    if rotation != 0 {
+      t.Log("The rotation function didn't recognize an overflow value")
+      t.Fail()
+    }
+}
+
+func TestPowerFunctionWorks(t *testing.T) {
+  value := pow(2, 2)
+  if value != 4 {
+    t.Log("The power function is not working correctly")
+    t.Fail()
+  }
+}
+
+func TestDateChangerProperlyConvertDateIntoFourCharacterString(t *testing.T) {
+    e := New()
+    e.Date = "01012016"
+    lastFourDigits := e.dateConv()
+    if lastFourDigits != "4256" {
+      t.Log("The date convertor isnt working quite right!", lastFourDigits)
+      t.Fail()
+    }
+}
+
+func TestAOffsetOnAGivenIndex(t *testing.T) {
+    e := New()
+    e.Date = "01012016"
+    offset := e.Offset(0)
+    if offset != 4 {
+      t.Log("Offset doesn't work on the 0 index given")
+      t.Fail()
+    }
+}
+
+func TestOffsetCanNotGoOutOfBoundsOnItsIndex(t *testing.T) {
+    e := New()
+    e.Date = "01012016"
+    offset := e.Offset(5)
+    if offset != 0 {
+        t.Log("Offset is not going to 0 when you go out of bounds on the string")
+        t.Fail()
+    }
+}
